26aad4467dc9bfb46d612e864c77826d839c4e8e
Baldazza Louka <louka.baldazza@viacesi.fr>
2021-08-24 14:47:49 +0200

    Door Animation

---
+++ Assets/DoorClick.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorClick : MonoBehaviour
+{
+    private bool isOpen;
+    private Animator animator;
+    private Coroutine currentRoutine;
+    private const float duration = 7;
+
+
+    private void Awake()
+    {
+        animator = transform.root.gameObject.GetComponent<Animator>();
+    }
+    public void OnMouseDown()
+    {
+        if(currentRoutine != null)
+        {
+            StopCoroutine(currentRoutine);
+        }
+        currentRoutine = StartCoroutine(RotateCoroutine());
+        isOpen = !isOpen;
+    }
+
+    private IEnumerator RotateCoroutine()
+    {
+
+        for (float f = 0; f <= duration; f += Time.deltaTime)
+        {
+            float lerp = Mathf.Lerp(animator.GetFloat("Blend"), isOpen ? 0 : 1, f/duration);
+            animator.SetFloat("Blend", lerp);
+            yield return null;
+        }
+    }
+
+}
+
